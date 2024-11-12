f11743b44d234ad696a503f5c6d1c9cec757dacf
byiringirogermain250 <ug2321411@ines.ac.rw>
2024-11-12 20:57:54 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Welcome to the Top Board Games Shop!");
+    // Add any interactive features here, such as category filtering
+    document.getElementById("price-catan").innerText = "$110";
+document.getElementById("price-risk").innerText = "$140";
+
+});
